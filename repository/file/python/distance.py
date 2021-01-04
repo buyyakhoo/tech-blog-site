cea327b2d002392f8527fe7d0bccb4222169fc48
buyyakhoo <pollapat13799@gmail.com>
2021-01-04 16:30:08 +0700

    delete repository

--- repository/file/python/distance.py
+++
@@ -1,7 +0,0 @@
-import math
-
-x1, y1 = eval(input("x1, y1: "))
-x2, y2 = eval(input("x2, y2: "))
-
-distance = math.sqrt(((x2-x1)**2) + ((y2-y1)**2))
-print(f'Distance is {distance}')
--- repository/file/python/mul.py
+++
@@ -1,5 +0,0 @@
-num = int(input()) #put some number to show the multiplication table 
-
-for i in range(12):
-    i += 1
-    print(f"{num} x {i} = {num*i}")
