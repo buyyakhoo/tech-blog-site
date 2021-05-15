c9ffef3dbe8ed0c9f08fef63d0ac3b2338a345d1
buyyakhoo <pollapat13799@gmail.com>
2021-05-16 00:04:23 +0700

    16/5/21

--- script/typing.js
+++ script/typing.js
@@ -6,7 +6,7 @@ var speed = 150;
 function typeWriter() {
     if (i < txt.length) {
         if (i == -1) {
-            document.getElementById("typing").innerHTML = " ";
+            document.getElementById("typing").innerHTML = "";
             i++;
             setTimeout(typeWriter, speed);
         } else {
